c69e4f8000c44f5ebeed6871373ce7821c312063
patelhapy <2019095900007053@spu.ac.in>
2022-01-07 12:58:41 +0530

    submit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x:");
+            String x = Console.ReadLine();
+            int xInt = Convert.ToInt32(x); // Int32.Parse(x);
+            Console.WriteLine("x = " + sum(xInt));
+
+            Console.Read();
+        }
+       static int sum(int x2)
+        {
+            return x2 * 2;
+        }
+    }
+}
